43b9b5c386d24de8ee28e28a9487f364af808298
‪Omar Mahmoud Abd-Ellatief‬‏ <113290018+Omarsanosy24@users.noreply.github.com>
2025-01-12 15:50:46 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,7 @@
 pip freeze > requirements.txt
 python manage.py makemigrations
 python manage.py migrate
+## يستخدم في حالة اذا كانت الترجمة موجودة و gettext
 #python manage.py makemessages -l ar
 #python manage.py compilemessages
 python manage.py runserver
